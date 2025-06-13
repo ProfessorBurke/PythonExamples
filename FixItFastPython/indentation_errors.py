6f8027404c3ebcd1c1b1de71eea89c65573a8df8
Maggie Burke <msb@alumni.brown.edu>
2025-06-13 14:02:19 -0400

    Add files via upload

---
+++ FixItFastPython/indentation_errors.py
@@ -0,0 +1,29 @@
+### Example 1
+##print("Hello world")
+##  print("Goodbye, indentation errors!")
+
+### Example 2
+##keep_going: str = input("Do you want to continue?  (yes or no): ")
+##if keep_going == "yes"
+##print("Okay, we'll keep going.")
+
+### Example 3
+##def convert_temp(temp_f: float)-> float:
+##    temp_c: float = (temp_f - 32) * 5/9
+##return temp_c
+##print(convert_temp(212))
+
+### Example 4
+##def convert_temp(temp_f: float)-> float:
+##    temp_c: float
+##    
+##temp_c = (temp_f - 32) * 5/9
+##    return temp_c
+##print(convert_temp(212))
+
+### Example 5 (from Notepad++)
+##keep_going: str = input("Do you want to continue?  (yes or no): ")
+##if keep_going == "yes":
+##    print("Okay, we'll keep going.")
+##	print("And going, and going...")
+
